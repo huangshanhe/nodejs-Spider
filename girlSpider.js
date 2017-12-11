c470e142df2d1de46511b5c3a4e6bdad4d449bc1
黄山河 <huangsh@guahao.com>
2017-12-11 17:28:31 +0800

    多页下载

--- girlSpider.js
+++ girlSpider.js
@@ -6,9 +6,6 @@ const cheerio = require("cheerio");
 const mkdirp = require('mkdirp');
 const async = require('async');
 
-// 目标网址
-let url = 'http://desk.zol.com.cn/meinv/1920x1080/1.html';
-
 // 本地存储目录
 let dir = './girlImages';
 
@@ -24,25 +21,6 @@ mkdirp(dir, function(err) {
     }
 });
 
-// 发送请求
-request(url, function(error, response, body) {
-    if(!error && response.statusCode == 200) {
-        let $ = cheerio.load(body);
-        $('.photo-list-padding a img').each(function() {
-            let src = $(this).attr('src');
-            src = src.replace(/t_s208x130c5/, 't_s960x600c5');
-            links.push(src);
-        });
-        async.mapLimit(links, 5, function(item, callback) {
-            download(item, dir, Math.floor(Math.random()*100000) + item.substr(-4,4));
-            console.log('成功下载图片'+item.substr(-10,10));
-            callback(null, item);
-        }, function(err, results) {
-            console.log('这页全部爬完。');
-        });
-    }
-});
-
 // 下载方法
 let download = function(url, dir, filename){
     request.head(url, function(err, res, body){
@@ -51,3 +29,34 @@ let download = function(url, dir, filename){
         }).pipe(fs.createWriteStream(dir + "/" + filename));
     });
 };
+
+function requestMore(url,index){
+    // 发送请求
+    request(url, function(error, response, body) {
+        if(!error && response.statusCode == 200) {
+            let $ = cheerio.load(body);
+            $('.photo-list-padding a img').each(function() {
+                let src = $(this).attr('src');
+                src = src.replace(/t_s208x130c5/, 't_s960x600c5');
+                links.push(src);
+            });
+            async.mapLimit(links, 5, function(item, callback) {
+                download(item, dir, Math.floor(Math.random()*100000) + item.substr(-4,4));
+                console.log('成功下载图片'+item.substr(-10,10));
+                callback(null, item);
+            }, function(err, results) {
+                console.log('第'+index+'页全部爬完。');
+            });
+        }
+    });
+}
+
+// 目标网址
+let urls = [];
+for(let i=1;i<2;i++){//push前X页
+    urls.push('http://desk.zol.com.cn/meinv/1920x1080/'+i+'.html');
+}
+
+for(let i=0;i<urls.length;i++){
+    requestMore(urls[i],i+1);
+}
--- tiebaSpider.js
+++ tiebaSpider.js
@@ -6,9 +6,6 @@ const cheerio = require("cheerio");
 const mkdirp = require('mkdirp');
 const async = require('async');
 
-// 目标网址
-let url = 'https://tieba.baidu.com/p/4705665446?pn=1';
-
 // 本地存储目录
 let dir = './tiebaImages';
 
@@ -24,27 +21,6 @@ mkdirp(dir, function(err) {
     }
 });
 
-// 发送请求
-request(url, function(error, response, body) {
-    if(!error && response.statusCode == 200) {
-        let $ = cheerio.load(body);
-        $('.BDE_Image').each(function(index) {
-            if(index<50){//控制下载数量
-                let src = $(this).attr('src');
-                links.push(src);
-            }
-        });
-        // 限制单ip请求并发 5个一次
-        async.mapLimit(links, 5, function(item, callback) {
-            download(item, dir, Math.floor(Math.random()*100000) + item.substr(-4,4));//随机数防止图片重名
-            console.log('成功下载图片'+item.substr(-10,10));
-            callback(null, item);
-        }, function(err, results) {
-            console.log('这页全部爬完。');
-        });
-    }
-});
-
 // 下载方法
 let download = function(url, dir, filename){
     request.head(url, function(err, res, body){
@@ -53,3 +29,36 @@ let download = function(url, dir, filename){
         }).pipe(fs.createWriteStream(dir + "/" + filename));
     });
 };
+
+function requestMore(url,index){
+    // 发送请求
+    request(url, function(error, response, body) {
+        if(!error && response.statusCode == 200) {
+            let $ = cheerio.load(body);
+            $('.BDE_Image').each(function(index) {
+                if(index<50){//控制下载数量
+                    let src = $(this).attr('src');
+                    links.push(src);
+                }
+            });
+            // 限制单ip请求并发 5个一次
+            async.mapLimit(links, 5, function(item, callback) {
+                download(item, dir, Math.floor(Math.random()*100000) + item.substr(-4,4));//随机数防止图片重名
+                console.log('成功下载图片'+item.substr(-10,10));
+                callback(null, item);
+            }, function(err, results) {
+                console.log('第'+index+'页全部爬完。');
+            });
+        }
+    });
+}
+
+// 目标网址
+let urls = [];
+for(let i=1;i<3;i++){//push前X页
+    urls.push('https://tieba.baidu.com/p/4705665446?pn='+i);
+}
+
+for(let i=0;i<urls.length;i++){
+    requestMore(urls[i],i+1);
+}
